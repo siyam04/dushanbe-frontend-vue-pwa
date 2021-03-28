faf82519e5263430a93831c35f5610a00dc6910b
siyam04 <galib.abdullah04@gmail.com>
2021-03-28 12:47:25 +0600

    GET API calls removed from service-worker.js

--- dist/service-worker.js
+++ dist/service-worker.js
@@ -9,61 +9,61 @@ workbox.setConfig({
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 
-/* Bill List (GET): https://dushanbe-backend-apis.herokuapp.com/api/bills/ */
-workbox.routing.registerRoute(
-    "https://dushanbe-backend-apis.herokuapp.com/api/bills/",
-    new workbox.strategies.NetworkFirst({
-        cacheName: "bills",
-        plugins: [
-            new workbox.expiration.Plugin({
-                maxAgeSeconds: 10 * 60 // 10 minutes
-            })
-        ],
-        method: "GET"
-    })
-);
-
-/* Type List (GET): https://dushanbe-backend-apis.herokuapp.com/api/types/ */
-workbox.routing.registerRoute(
-    "https://dushanbe-backend-apis.herokuapp.com/api/types/",
-    new workbox.strategies.NetworkFirst({
-        cacheName: "types",
-        plugins: [
-            new workbox.expiration.Plugin({
-                maxAgeSeconds: 10 * 60 // 10 minutes
-            })
-        ],
-        method: "GET"
-    })
-);
-
-/* Material List (GET): https://dushanbe-backend-apis.herokuapp.com/api/materials/ */
-workbox.routing.registerRoute(
-    "https://dushanbe-backend-apis.herokuapp.com/api/materials/",
-    new workbox.strategies.NetworkFirst({
-        cacheName: "materials",
-        plugins: [
-            new workbox.expiration.Plugin({
-                maxAgeSeconds: 10 * 60 // 10 minutes
-            })
-        ],
-        method: "GET"
-    })
-);
-
-/* Work Submission List (GET): https://dushanbe-backend-apis.herokuapp.com/api/work-submissions/ */
-workbox.routing.registerRoute(
-    "https://dushanbe-backend-apis.herokuapp.com/api/work-submissions/",
-    new workbox.strategies.NetworkFirst({
-        cacheName: "work-submissions",
-        plugins: [
-            new workbox.expiration.Plugin({
-                maxAgeSeconds: 10 * 60 // 10 minutes
-            })
-        ],
-        method: "GET"
-    })
-);
-
-
+// /* Bill List (GET): https://dushanbe-backend-apis.herokuapp.com/api/bills/ */
+// workbox.routing.registerRoute(
+//     "https://dushanbe-backend-apis.herokuapp.com/api/bills/",
+//     new workbox.strategies.NetworkFirst({
+//         cacheName: "bills",
+//         plugins: [
+//             new workbox.expiration.Plugin({
+//                 maxAgeSeconds: 10 * 60 // 10 minutes
+//             })
+//         ],
+//         method: "GET"
+//     })
+// );
+//
+// /* Type List (GET): https://dushanbe-backend-apis.herokuapp.com/api/types/ */
+// workbox.routing.registerRoute(
+//     "https://dushanbe-backend-apis.herokuapp.com/api/types/",
+//     new workbox.strategies.NetworkFirst({
+//         cacheName: "types",
+//         plugins: [
+//             new workbox.expiration.Plugin({
+//                 maxAgeSeconds: 10 * 60 // 10 minutes
+//             })
+//         ],
+//         method: "GET"
+//     })
+// );
+//
+// /* Material List (GET): https://dushanbe-backend-apis.herokuapp.com/api/materials/ */
+// workbox.routing.registerRoute(
+//     "https://dushanbe-backend-apis.herokuapp.com/api/materials/",
+//     new workbox.strategies.NetworkFirst({
+//         cacheName: "materials",
+//         plugins: [
+//             new workbox.expiration.Plugin({
+//                 maxAgeSeconds: 10 * 60 // 10 minutes
+//             })
+//         ],
+//         method: "GET"
+//     })
+// );
+//
+// /* Work Submission List (GET): https://dushanbe-backend-apis.herokuapp.com/api/work-submissions/ */
+// workbox.routing.registerRoute(
+//     "https://dushanbe-backend-apis.herokuapp.com/api/work-submissions/",
+//     new workbox.strategies.NetworkFirst({
+//         cacheName: "work-submissions",
+//         plugins: [
+//             new workbox.expiration.Plugin({
+//                 maxAgeSeconds: 10 * 60 // 10 minutes
+//             })
+//         ],
+//         method: "GET"
+//     })
+// );
+//
+//
 
