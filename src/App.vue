789145a337b41351e4830e68fe88387a5df08157
joy210 <sohanoor404@gmail.com>
2021-06-17 01:03:32 +0600

    update style

--- src/App.vue
+++ src/App.vue
@@ -10,7 +10,41 @@ export default {
 </script>
 
 <style>
+:root {
+  --primaryColor: #0061a7;
+}
+
+.bgPrimary {
+  background: var(--primaryColor);
+}
+
 body {
   background: #fafafa !important;
+}
+
+input:focus,
+select:focus {
+  box-shadow: none !important;
+  outline: none !important;
+}
+
+/* width */
+::-webkit-scrollbar {
+  width: 6px;
+}
+
+/* Track */
+::-webkit-scrollbar-track {
+  background: #f1f1f1;
+}
+
+/* Handle */
+::-webkit-scrollbar-thumb {
+  background: #888;
+}
+
+/* Handle on hover */
+::-webkit-scrollbar-thumb:hover {
+  background: var(--primaryColor);
 }
 </style>
